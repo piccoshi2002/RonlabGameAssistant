6a9617f702d476a6fc48bfd19898e9be3e69119b
piccoshi2002 <piccoshi2002@gmail.com>
2026-05-08 21:56:27 -0500

    Update WorldSettings.java

--- src/main/java/com/ronlab/rga/world/WorldSettings.java
+++ src/main/java/com/ronlab/rga/world/WorldSettings.java
@@ -1,5 +1,6 @@
 package com.ronlab.rga.world;
 
+import org.bukkit.Difficulty;
 import org.bukkit.GameMode;
 import org.bukkit.World;
 
@@ -8,14 +9,18 @@ public class WorldSettings {
     private final GameMode gamemode;
     private final boolean pvp;
     private final World.Environment environment;
+    private final Difficulty difficulty;
 
-    public WorldSettings(GameMode gamemode, boolean pvp, World.Environment environment) {
+    public WorldSettings(GameMode gamemode, boolean pvp,
+                         World.Environment environment, Difficulty difficulty) {
         this.gamemode = gamemode;
         this.pvp = pvp;
         this.environment = environment;
+        this.difficulty = difficulty;
     }
 
     public GameMode getGamemode() { return gamemode; }
     public boolean isPvp() { return pvp; }
     public World.Environment getEnvironment() { return environment; }
+    public Difficulty getDifficulty() { return difficulty; }
 }
